16abb59a7e3a28188620269ec7ef4dd70fac9253
JC MISA <johncarlomisa399@gmail.com>
2024-07-26 15:00:48 +0800

    add functionality to delete budget and expenses connected to that budget

--- app/(routes)/dashboard/expenses/[budgetId]/page.jsx
+++ app/(routes)/dashboard/expenses/[budgetId]/page.jsx
@@ -3,15 +3,32 @@
 import AddExpense from "@/components/custom/AddExpense";
 import BudgetItem from "@/components/custom/BudgetItem";
 import ExpenseList from "@/components/custom/ExpenseList";
+import { Button } from "@/components/ui/button";
 import { db } from "@/utils/db";
 import { Budgets, Expenses } from "@/utils/schema";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { desc, eq, getTableColumns, sql } from "drizzle-orm";
+import { Trash } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { useRouter } from "next/navigation";
+
 
 const Expense = ({ params }) => {
   const { user } = useUser();
+  const route = useRouter();
+
   const [budgetInfo, setBudgetInfo] = useState({})
   const [expenseList, setExpenseList] = useState([])
 
@@ -79,8 +96,64 @@ const Expense = ({ params }) => {
       </p>)
     }
   }
+
+  const deleteBudget = async () => {
+    try {
+      // we first delete the child record in order to not violate the foreign key constraints, because some of the records in Expenses are connected to Budgets table through budgetId property of Expenses and id proerty of Budgets
+      const deleteExpenseRes = await db.delete(Expenses)
+        .where(eq(Expenses.budgetId, params.budgetId))
+        .returning({ deletedExpense: Expenses.budgetId })
+
+      if (deleteExpenseRes) {
+        toast(<p className="text-sm font-bold text-green-500">
+          Expense with id {deleteExpenseRes.deletedExpense} was deleted successfully
+        </p>)
+
+        const deleteBudgetRes = await db.delete(Budgets)
+          .where(eq(Budgets.id, params.budgetId))
+          .returning({ deletedBudget: Budgets.id })
+
+        if (deleteBudgetRes) {
+          toast(<p className="text-sm font-bold text-green-500">
+            Budget with id {deleteBudgetRes.deletedBudget} was deleted successfully
+          </p>)
+
+          route.replace('/dashboard/budget')
+        }
+      }
+    } catch (error) {
+      toast(<p className="text-sm font-bold text-red-500">
+        Internal error occured while deleting the records
+      </p>)
+    }
+  }
+
   return <div className="p-5">
-    <h2 className="text-2xl font-bold">My Expenses</h2>
+    <h2 className="text-2xl font-bold flex justify-between items-center">
+      My Expenses
+
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button className="flex gap-2" variant="destructive">
+            <Trash /> Delete
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your budget along with the expenses.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => deleteBudget()}>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+    </h2>
+
     <div className="grid grid-cols-1 md:grid-cols-2 mt-7 gap-5">
       {budgetInfo ? <BudgetItem budget={budgetInfo} /> : <div className="w-full bg-dark rounded-lg h-[150px] animate-pulse"></div>}
       <AddExpense refreshData={() => getBudgetInfo()} budgetId={params.budgetId} />
